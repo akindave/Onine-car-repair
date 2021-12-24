ee7dcaee230c050c5983e036c88f1f7dfbd5dcee
paminus-kingori <pkmurungi.pm@gmail.com>
2021-12-24 11:45:31 +0300

    homepage mpya

---
+++ home.php
@@ -0,0 +1,254 @@
+<?php include 'php/header.php';?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0,user-scalable=no">
+   <meta name="keywords" content="">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="description" content="mechLocator | online mechanic locator">
+   <meta name="author" content="paminus">
+    <title>mechLocator | Homepage</title>
+    <link rel="stylesheet" href="css/home.css">
+    <link rel="stylesheet" href="css/nav.css">
+</head>
+<body>
+    <main class="home_main">
+        <section class="landing-sect flex-wrap">
+            <article class="sect-text">
+                <h2 class="sect-title white-text">
+                    What is mechLocator?
+                </h2>
+                <p class="white-text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas, veritatis fuga magni, ut adipisci reprehender
+                </p>
+                <div class="landing-btns">
+                <a href="signup.php">
+                <button class="add-btn reg-btn">Register now for free</button>
+                </a>
+            </div>
+            </article>
+            <picture class="img-holder flex-wrap">
+                <img src="Images/fly.svg" alt="grow with us" class="landing-img">
+            </picture>
+
+            <div class="scroll">
+                <div class="rectangle">
+                    <img src="icons/arrow-down-white.svg" alt="down" class="icon arrow-icon">
+                </div>
+                <p>
+                    Scroll down
+                </p>
+            </div>
+        </section>
+
+    <section class="mech-locate flex-wrap">
+        <aside class="svg_holder">
+                    <img src="Images/seo-edited.svg" alt="search for a mech" class="svg_illustration">
+        </aside>
+
+        <article class="resources_text">
+                    <h3 class="article-text">
+                        Find a mechanic near you
+                    </h3>
+                    <p class="margin20">
+                       lorem ipsum dolor sit amet, consectetur adip
+                       Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic illum accusantium autem expedita est quaerat ab animi labo
+                    </p>
+                    <a href="mechNear.php" class="card">
+                        Mechanics near me
+                    </a>
+                </article>
+    </section>
+
+    <section class="records-sect flex-wrap wrap-reverse">
+        <article class="resources_text">
+                    <h3 class="article-text">
+                        Keep track of your car repairs
+                    </h3>
+                    <p class="margin20">
+                       lorem ipsum dolor sit amet, consectetur adip
+                       Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic illum accusantium autem expedita est quaerat ab animi labo
+                    </p>
+                    <a href="repairHistory.php" class="card">
+                        View records
+                    </a>
+                </article>
+    
+        <aside class="svg_holder">
+                    <img src="Images/notes.svg" alt="take note" class="svg_illustration">
+        </aside>
+    </section>
+
+    <section class="mech-locate flex-wrap">
+        <aside class="svg_holder">
+                    <img src="Images/mech1.jpg" alt="search for a mech" class="svg_illustration">
+        </aside>
+
+        <article class="resources_text">
+                    <h3 class="article-text">
+                        Apply to join us as a mechanic
+                    </h3>
+                    <p class="margin20">
+                      Boost your customer reach by using our platform and increase your revenue. 
+                      Application is easy and free.
+                    </p>
+                    <a href="mech-signup.php" class="card">
+                        Apply now
+                    </a>
+        </article>
+    </section>
+
+    <section class="review swiper-container">
+       <h3 class="review-title listing-title">
+           Don't believe us, trust what our customers have said.
+       </h3>
+
+       <div class="swiper-wrapper customer-sect">
+
+           <!-- joy customer  -->
+           <article class="swiper-slide cust-card">
+               <div class="box-circle"> <div class="semi-circle"></div> </div>
+               <img src="Images/lady-mech.jpg" alt="Joy landlady" class="cust-img">
+               <div class="cust-desc">
+                   <img src="icons/quote.svg" alt="quote" class="quote-svg">
+                   <p class="cust-text">
+                       Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, eos laboriosam nemo reprehenderit
+                   </p>
+                   <div class="flex-wrap spaceBetween">
+                       <aside class="stars flex-wrap">
+                           <img src="icons/orange-Star.svg" alt="star" class="star-icon">
+                           <img src="icons/orange-Star.svg" alt="star" class="star-icon">
+                           <img src="icons/orange-Star.svg" alt="star" class="star-icon">
+                           <img src="icons/orange-Star.svg" alt="star" class="star-icon">
+                           <img src="icons/orange-Star.svg" alt="star" class="star-icon">
+
+                       </aside>
+                       <p class="cust-name">
+                           ~ Joy Mutugi
+                       </p>
+                   </div>
+               </div>
+           </article>
+
+            <!-- joy customer  -->
+            <article class="swiper-slide cust-card">
+               <div class="box-circle"> <div class="semi-circle"></div> </div>
+               <img src="Images/lady-mech.jpg" alt="Joy landlady" class="cust-img">
+               <div class="cust-desc">
+                   <img src="icons/quote.svg" alt="quote" class="quote-svg">
+                   <p class="cust-text">
+                       Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, eos laboriosam nemo reprehenderit
+                   </p>
+                   <div class="flex-wrap spaceBetween">
+                       <aside class="stars flex-wrap">
+                           <img src="icons/orange-Star.svg" alt="star" class="star-icon">
+                           <img src="icons/orange-Star.svg" alt="star" class="star-icon">
+                           <img src="icons/orange-Star.svg" alt="star" class="star-icon">
+                           <img src="icons/orange-Star.svg" alt="star" class="star-icon">
+                           <img src="icons/orange-Star.svg" alt="star" class="star-icon">
+
+                       </aside>
+                       <p class="cust-name">
+                           ~ Joy Mutugi
+                       </p>
+                   </div>
+               </div>
+           </article>
+
+            <!-- joy customer  -->
+            <article class="swiper-slide cust-card">
+               <div class="box-circle"> <div class="semi-circle"></div> </div>
+               <img src="Images/lady-mech.jpg" alt="Joy landlady" class="cust-img">
+               <div class="cust-desc">
+                   <img src="icons/quote.svg" alt="quote" class="quote-svg">
+                   <p class="cust-text">
+                       Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, eos laboriosam nemo reprehenderit
+                   </p>
+                   <div class="flex-wrap spaceBetween">
+                       <aside class="stars flex-wrap">
+                           <img src="icons/orange-Star.svg" alt="star" class="star-icon">
+                           <img src="icons/orange-Star.svg" alt="star" class="star-icon">
+                           <img src="icons/orange-Star.svg" alt="star" class="star-icon">
+                           <img src="icons/orange-Star.svg" alt="star" class="star-icon">
+                           <img src="icons/orange-Star.svg" alt="star" class="star-icon">
+
+                       </aside>
+                       <p class="cust-name">
+                           ~ Joy Mutugi
+                       </p>
+                   </div>
+               </div>
+           </article>
+       </div>
+
+   <div class="swiper-pagination"></div>
+
+    </section>
+    </main>
+
+    <footer class="flex-wrap">
+        <div class="bg-footer"></div>
+    
+    <aside class="quick-links">
+        <h4>Quick Links</h4>
+        <ul>
+            <li>Home</li>
+            <li>Mechanic near me</li>
+            <li>My repair records</li>
+            <li>Add a repair record</li>
+            <li>Apply to be a mechanic</li>
+            <li>My profile</li>
+        </ul>
+    </aside>
+    <aside class="mission">
+        <article class="mission-text">
+            <h4>Vision</h4>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ercitationem,
+            tempore vero dolore velit providentdis odit aliquid voluptates eos id!
+        </article>
+        <article class="vision-text">
+            <h4>Mission</h4>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ercitationem,
+            tempore vero dolore velit providentdis odit aliquid voluptates eos id!
+        </article>
+    </aside>
+    
+    <aside class="media-handles">
+      <h4>Connect with us on our social media accounts</h4>
+      <aside class="socio-links">
+         <a href="" class="flex-wrap"><img src="icons/twitter.svg" alt="twitter handle" class="icon">Twitter</a>
+         <a href="" class="flex-wrap"><img src="icons/facebook.svg" alt="facebook page" class="icon"> Facebook</a>
+         <a href="" class="flex-wrap"><img src="icons/IG.svg" alt="instagram" class="icon"> Instagram</a>
+      </aside>
+
+    
+    </aside>
+
+</footer>
+
+</body>
+<script src="js/nav.js" charset="utf-8"></script>
+<script src="js/swiperJs/swiper.min.js" charset="utf-8"></script>
+<link rel="stylesheet" href="js/swiperJs/swiper.min.css">
+<link rel="stylesheet" href="css/footer.css">
+<script>
+      let swiper = new Swiper('.review', {
+   slidesPerView: 'auto',
+   spaceBetween: 15,
+   centeredSlides: false,
+   watchSlidesVisibility: true,
+   watchSlidesProgress: true,
+   freeMode: true,
+   pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+   },
+  });
+
+</script>
+
+<noscript>Your browser does not support JavaScript!</noscript>
+
+</html>
